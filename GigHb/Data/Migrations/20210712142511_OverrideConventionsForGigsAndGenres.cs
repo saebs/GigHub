861b1f2494949e1ac7f32fb2f14d9207a2650942
sabelo <ntabenism@gmail.com>
2021-07-12 16:32:51 +0200

    bgu fixes after refactoring projectwide

---
+++ GigHb/Data/Migrations/20210712142511_OverrideConventionsForGigsAndGenres.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GigHb.Data.Migrations
+{
+    public partial class OverrideConventionsForGigsAndGenres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Venue",
+                table: "Gigs",
+                type: "TEXT",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genres",
+                type: "TEXT",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Venue",
+                table: "Gigs",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genres",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 255);
+        }
+    }
+}
---
+++ GigHb/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace GigHb.Models
+{
+
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<IdentityResult> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager.CreateAsync(this);
+            return userIdentity;
+        }
+        
+    }
+}
