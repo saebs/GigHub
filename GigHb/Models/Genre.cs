3269b2ea8226c90c03510c2ce7a2bfa419bd8c5c
sabelo <ntabenism@gmail.com>
2021-07-13 15:19:59 +0200

    verify this

--- GigHb/Models/Genre.cs
+++ GigHb/Models/Genre.cs
@@ -5,6 +5,8 @@ namespace GigHb.Models
     public class Genre
     {
         public byte Id { get; set; }
+        
+        // need to verify is constraints where applied
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
