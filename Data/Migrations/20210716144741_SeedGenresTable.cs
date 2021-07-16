ed17482ea07c68cef02e6ba6dbebb74462925b3d
sabelo <ntabenism@gmail.com>
2021-07-16 16:58:36 +0200

    Seed Gengres Table

---
+++ Data/Migrations/20210716144741_SeedGenresTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GigHub.Data.Migrations
+{
+    public partial class SeedGenresTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Jazz')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (2, 'RnB')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (3, 'Alternative Rock')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (4, 'Pop')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (5, 'Afro Soul')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (6, 'Amapiano')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE * FROM Genres");
+        }
+    }
+}
