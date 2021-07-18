1241ffd10f75e5922958faa644b686a39ad321d4
sabelo <ntabenism@gmail.com>
2021-07-18 15:26:07 +0200

    Refactor - Add GigForm View Model instead for client and UI Usability purposes

---
+++ ViewModels/GigFormViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+    }
+}
