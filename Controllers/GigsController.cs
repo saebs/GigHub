1b6c4bc6d1fda715377e0d8f221f87f863921b52
sabelo <ntabenism@gmail.com>
2021-07-18 16:19:21 +0200

    reviewed and cleaned up

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -1,16 +1,25 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using GigHub.Data;
+using GigHub.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Create()
         {
-            return View();
+            var viewGigFormModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            }; 
+            return View(viewGigFormModel);
         }
     }
 }
