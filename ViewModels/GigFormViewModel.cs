ef9f3899735788e8cd5507d9335bf6631ff34fce
sabelo <ntabenism@gmail.com>
2021-07-19 09:06:56 +0200

    Name Method to self Document, Annotate view Model for UI

--- ViewModels/GigFormViewModel.cs
+++ ViewModels/GigFormViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using GigHub.Models;
 
 namespace GigHub.ViewModels
@@ -8,7 +9,10 @@ namespace GigHub.ViewModels
         public string Venue { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
-        public int Genre { get; set; }
+        
+        [Display(Name = "Genre")]
+        public int GenreId { get; set; }
+        
         public IEnumerable<Genre> Genres { get; set; }
     }
 }
